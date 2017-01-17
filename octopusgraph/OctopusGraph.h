035387b743667d33744d4d51ece2812324c8a949
Tim Hemel <tim@securesoftware.nl>
2017-01-17 11:18:42 +0100

    change function defs to C++-book conventions, move constructor, add OctopusGraph  class.

---
+++ octopusgraph/OctopusGraph.h
@@ -0,0 +1,7 @@
+
+namespace OctopusGraph {
+
+	class OctopusGraph {
+	};
+}
+
--- octopusgraph/OctopusGraphPass.cpp
+++ octopusgraph/OctopusGraphPass.cpp
@@ -2,30 +2,42 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
+#include "OctopusGraph.h"
+
 using namespace llvm;
 
 namespace {
 	struct OctopusGraphPass : public FunctionPass {
 		static char ID;
-		// implement destructor
 
-		OctopusGraphPass() : FunctionPass(ID) { }
+		OctopusGraphPass();
 
 		virtual bool runOnFunction(Function &F);
+
+	private:
+
+		OctopusGraph::OctopusGraph octopus_graph;
 
 	};
 }
 
 char OctopusGraphPass::ID = 0;
 
-bool OctopusGraphPass::runOnFunction(Function &F) {
+OctopusGraphPass::OctopusGraphPass() : FunctionPass(ID)
+{
 }
 
 
-static void registerOctopusGraphPass(const PassManagerBuilder &, legacy::PassManagerBase &PM) {
+bool OctopusGraphPass::runOnFunction(Function &F)
+{
+	return false;
+}
+
+
+static void registerOctopusGraphPass(const PassManagerBuilder &, legacy::PassManagerBase &PM)
+{
 	PM.add(new OctopusGraphPass());
 }
 
-// rename?
 static RegisterStandardPasses RegisterOctopusGraphPass(PassManagerBuilder::EP_EarlyAsPossible, registerOctopusGraphPass);
 
