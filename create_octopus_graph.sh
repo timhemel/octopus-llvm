ecca13356035b8e7d75fb849e22d7c0a42e2e6d0
Tim Hemel <tim@securesoftware.nl>
2017-01-20 21:02:13 +0100

    Add -w to suppress warnings

--- create_octopus_graph.sh
+++ create_octopus_graph.sh
@@ -7,5 +7,5 @@ basedir=`dirname "$0"`
 # Compile a C module and output graphviz
 # -mllvm -noddg  # do not generate data dependence edges
 # -mllvm -noloc  # do not generate location nodes and edges
-clang -g -O0 -c -Xclang -load -Xclang "$basedir"/build/octopusgraph/libOctopusGraphPass.so -mllvm -graphviz -o /dev/null "$@"
+clang -w -g -O0 -c -Xclang -load -Xclang "$basedir"/build/octopusgraph/libOctopusGraphPass.so -mllvm -graphviz -o /dev/null "$@"
 
